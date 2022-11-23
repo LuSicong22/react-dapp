0c00d36a82b45f8e0fbeffd826b70d3bc3cd3850
simon <simon.lu@tron.network>
2022-11-23 11:13:27 +0800

    feat: add greater contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: "https://mainnet.infura.io/v3/ad9d6169a94f49298cfa89190e5a498e",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
